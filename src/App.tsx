3259e7382a9002439524ddf1af5eb1287ab42e91
John Doe <dgrishakin12@gmail.com>
2020-07-14 19:12:48 +0300

    Creating class "app-wrapper" in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-wrapper">
       <BrowserRouter>
         <Header />
         <Navbar />
